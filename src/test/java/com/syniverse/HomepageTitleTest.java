17276ee01f35c121b04df069f10f96c0a89dbe83
chaksamu <chaksamu@gmail.com>
2021-09-29 19:40:34 +0530

    maven.testng.selenium.jenkins_first_commit

---
+++ src/test/java/com/syniverse/HomepageTitleTest.java
@@ -0,0 +1,36 @@
+package com.syniverse;
+
+import java.util.ResourceBundle;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HomepageTitleTest {
+	//static String URL = "https://mysyniverse.syniverse.com/";
+			@Test
+			public void loginPageTitleTest() {
+				// TODO Auto-generated method stub
+				//System.setProperty("webdriver.chrome.driver", "C:\\Users\\g706427\\Box Sync\\eclipse\\chromedriver.exe");
+				ResourceBundle rb = ResourceBundle.getBundle("config");
+				String url = rb.getString("url");
+				String uname = rb.getString("username");
+				String passwd = rb.getString("password");
+				WebDriverManager.chromedriver().setup();
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+				//driver.get("https://mysyniverse.syniverse.com");
+				driver.get(url);
+				driver.findElement(By.id("username")).sendKeys(uname);
+				driver.findElement(By.id("password")).sendKeys(passwd);
+				driver.findElement(By.cssSelector("input[type='submit']")).click();
+				String title_h = driver.getTitle();
+				Assert.assertEquals(title_h,"MySyniverse Portal");
+				System.out.println(title_h);
+				driver.close();			
+			}
+}
