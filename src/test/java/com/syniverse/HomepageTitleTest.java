95aabf52d028f8c3b020da63ab8aa8158efe4fff
chaksamu <45867824+chaksamu@users.noreply.github.com>
2021-09-30 10:38:11 +0530

    Update HomepageTitleTest.java

--- src/test/java/com/syniverse/HomepageTitleTest.java
+++ src/test/java/com/syniverse/HomepageTitleTest.java
@@ -15,7 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class HomepageTitleTest {
 	//static String URL = "https://mysyniverse.syniverse.com/";
 			@Test
-			public void loginPageTitleTest() {
+			public void HpageTitleTest() {
 				// TODO Auto-generated method stub
 				//System.setProperty("webdriver.chrome.driver", "C:\\Users\\g706427\\Box Sync\\eclipse\\chromedriver.exe");
 				System.setProperty("java.awt.headless", "false");
@@ -28,9 +28,10 @@ public class HomepageTitleTest {
 				driver.manage().window().maximize();
 				//driver.get("https://mysyniverse.syniverse.com");
 				driver.get(url);
-				driver.findElement(By.id("username")).sendKeys(uname);
-				driver.findElement(By.id("password")).sendKeys(passwd);
-				driver.findElement(By.cssSelector("input[type='submit']")).click();
+				driver.findElement(By.id("login-email-field")).sendKeys(uname);
+				driver.findElement(By.id("login-password-field")).sendKeys(passwd);
+				driver.findElement(By.id("log-in-button")).click();
+				//driver.findElement(By.cssSelector("input[type='submit']")).click();
 				String title_h = driver.getTitle();
 				Assert.assertEquals(title_h,"MySyniverse Portal");
 				System.out.println(title_h);
