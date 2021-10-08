1e2be5d9f51ec1a292673eb7c0e56fe20fa41b64
chaksamu <chaksamu@gmail.com>
2021-10-08 21:27:49 +0530

    new test

---
+++ src/main/java/com/syniverse/App.java
@@ -0,0 +1,20 @@
+package com.syniverse;
+
+import java.util.ResourceBundle;
+
+public class App {
+
+	public int userLogin(String in_user, String in_pwd) {
+		
+		ResourceBundle rbc = ResourceBundle.getBundle("config");
+		String uname = rbc.getString("username");
+		String passwd = rbc.getString("password");
+		
+		if (in_user.equals(uname) && in_pwd.equals(passwd))
+			return 1;
+		else
+			return 0;
+			
+		
+	}
+}
